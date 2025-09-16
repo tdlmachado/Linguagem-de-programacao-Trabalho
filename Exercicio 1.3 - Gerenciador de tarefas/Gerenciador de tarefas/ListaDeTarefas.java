4e1aadb13e0b9eed4093d0420fd76921e6eb7e49
Win11 <tdlmachado@minha.fag.edu.br>
2025-09-16 19:41:20 -0300

    rename

--- Exercicio 1.3 - Gerenciador de tarefas/Gerenciador de tarefas/ListaDeTarefas.java
+++ Exercicio 1.3 - Gerenciador de tarefas/Gerenciador de tarefas/ListaDeTarefas.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class GerenciadorDeTarefas {
+public class ListaDeTarefas {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
