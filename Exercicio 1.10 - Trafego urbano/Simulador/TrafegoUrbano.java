233e13c0bf6ace2dbb6bc484656296cf44b357ed
Win11 <tdlmachado@minha.fag.edu.br>
2025-09-18 20:28:28 -0300

    18/09

---
+++ Exercicio 1.10 - Trafego urbano/Simulador/TrafegoUrbano.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class TrafegoUrbano {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int LimiteDeVelocidade = 60;
+
+        System.out.print("Digite sua velocidade: ");
+        double Velocidade = input.nextDouble();
+
+        System.out.print("--- Sinal de transito ---");
+        System.out.print("\n\t [Vermelho]");
+        System.out.print("\n\t [Amarelo]");
+        System.out.print("\n\t  [Verde]");
+        System.out.print("\n-------------------------\n");
+        System.out.print("Digite o sinal do semáforo: ");
+        String sinal = input.next().toLowerCase();
+        System.out.print("-------------------------\n");
+
+        if(Velocidade > LimiteDeVelocidade){
+            if (Velocidade <= LimiteDeVelocidade + 10){
+                System.out.println("\nMulta leve por excesso de velocidade!");
+            }else{
+                System.out.println("Multa grave por excesso de velocidade!");
+            }
+        }else{
+            System.out.println("Você está dentro do limite de velocidade!");
+        }
+
+    }
+}
