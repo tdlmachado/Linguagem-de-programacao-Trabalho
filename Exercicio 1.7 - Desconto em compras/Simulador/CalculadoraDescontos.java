bdde2521ac9e6b5a4cc4768bf6d24772624611d1
Thiago <thyagodelimamachado@gmail.com>
2025-09-17 22:57:25 -0300

    17/09

---
+++ Exercicio 1.7 - Desconto em compras/Simulador/CalculadoraDescontos.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class CalculadoraDescontos {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int Valor;
+        double ValorFinal;
+
+        System.out.print("Digite o Valor da sua compra: ");
+        Valor = input.nextInt();
+
+        if (Valor > 0 && Valor < 100) {
+            System.out.print("Sua compra não se enquadra em nosso desconto!");
+
+        }else if (Valor >= 100 && Valor < 500) {
+            ValorFinal = Valor + (Valor * 0.1);
+            System.out.println("Sua compra recebeu um desconto de 10% no valor");
+            System.out.print("Valor final: "+ValorFinal);
+
+        }else if (Valor >= 500) {
+            ValorFinal = Valor + (Valor * 0.2);
+            System.out.println("Sua compra recebeu um desconto de 20% no valor");
+            System.out.print("Valor final: "+ValorFinal);
+        }
+    }
+}
