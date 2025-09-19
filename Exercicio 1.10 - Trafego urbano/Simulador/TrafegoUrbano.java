9131d93ae92f6159000c006b31c68f9bdf606820
Thiago <thyagodelimamachado@gmail.com>
2025-09-18 21:15:28 -0300

    18/09

--- Exercicio 1.10 - Trafego urbano/Simulador/TrafegoUrbano.java
+++ Exercicio 1.10 - Trafego urbano/Simulador/TrafegoUrbano.java
@@ -28,5 +28,16 @@ public class TrafegoUrbano {
             System.out.println("Você está dentro do limite de velocidade!");
         }
 
+        switch (sinal){
+            case "vermelho":
+                System.out.println("--> O sinal está fechado, aguarde ele abrir!");
+                break;
+            case "amarelo":
+                System.out.println("--> O sinal está amarelo, diminua a velocidade!");
+                break;
+            case "verde":
+                System.out.println("--> O sinal está aberto, pode prosseguir!");
+                break;
+        }
     }
 }
